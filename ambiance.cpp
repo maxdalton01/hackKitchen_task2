cf935a523b86732bc682d55e53bb723a06941112
Maxwell Dalton <maxdalton6@gmail.com>
2021-05-08 14:47:19 -0700

    Ambiance now plays 5 times in background.

--- ambiance.cpp
+++ ambiance.cpp
@@ -4,14 +4,11 @@
 using namespace std;
 
 int main() {
-    string file = "ambiance.mp3";
-    string command = "while :; do afplay " + file + " &>/dev/null &; done";
-    system(command.c_str());
-    cout << "Sound is playing! do you like?" << endl;
-    string userInput;
-    getline(cin, userInput);
-    cout << "You said " + userInput << endl;
     
-    command = "killall afplay";
-    system(command.c_str());
+    string file = "ambiance.mp3";
+    
+    // trouble figuring out how to terminate background commands, so play only 5 times
+    // shell command for running afplay in a loop 5 times in the background
+    string command = "(for i in {1..5}; do afplay " + file + " &>/dev/null; done &) & RUNNING_PID=$!";
+    system(command.c_str()); // runs above command
 }
